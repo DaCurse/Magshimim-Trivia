ab0d97704fc0f4d083dd4aa8a6ace050fa6504ba
Daniel <dacurse0@gmail.com>
2019-06-18 15:53:09 +0300

    Added Communicator declaration

---
+++ TriviaServer/Communicator.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <winsock2.h>
+#include <map>
+#include "IRequestHandler.h"
+#include "RequestHandlerFactory.h"
+
+class Communicator
+{
+	public:
+		Communicator();
+		~Communicator();
+
+		void bindAndListen();
+		void handleRequests();
+
+	private:
+
+		std::map<SOCKET, IRequestHandler> m_clients;
+		RequestHandlerFactory m_factory;
+
+		void startThreadForNewClient();
+
+};
+
