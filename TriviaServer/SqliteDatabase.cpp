f82a4209c2135f64c0c71e431ef8c15ed12da320
Daniel <dacurse0@gmail.com>
2019-06-27 16:03:54 +0300

    Started working on fetching from database

--- TriviaServer/SqliteDatabase.cpp
+++ TriviaServer/SqliteDatabase.cpp
@@ -46,19 +46,87 @@ const void SqliteDatabase::signupUser(std::string username, std::string password
 
 void SqliteDatabase::sqlInsert(std::string table, std::vector<std::string> columns, std::vector<std::string> values)
 {
+
+	std::stringstream statementStream;
+	statementStream << "INSERT INTO " << table << " ";
+
 	std::ostringstream columnStringStream, valueStringStream;
 	std::copy(columns.begin(), columns.end(), std::ostream_iterator<std::string>(columnStringStream, ","));
 	std::copy(values.begin(), values.end(), std::ostream_iterator<std::string>(valueStringStream, "','"));
 	std::string columnString = columnStringStream.str(), valueString = valueStringStream.str();
-
-	std::string statement = "INSERT INTO users (" + columnString.substr(0, columnString.size() - 1) + ") VALUES('" + valueString.substr(0, valueString.size() - 2) + ");";
+	statementStream << columnString.substr(0, columnString.size() - 1) << ") VALUES('" << valueString.substr(0, valueString.size() - 2) << ");";
 
 	char* errMessage = nullptr;
-	int res = sqlite3_exec(m_db, statement.c_str(), nullptr, nullptr, &errMessage);
+	int res = sqlite3_exec(m_db, statementStream.str().c_str(), nullptr, nullptr, &errMessage);
 
 	if (res != SQLITE_OK)
 	{
 		throw std::runtime_error("Failed to insert into database: '" + std::string(errMessage) + "'");
 	}
 
+}
+
+void SqliteDatabase::sqlFetch(std::string table, std::vector<std::string> columns, std::string whereCondition, std::string orderBy, std::string groupBy, int limit, std::string extra)
+{
+	std::stringstream statementStream;
+	statementStream << "SELECT ";
+
+	std::ostringstream columnStringStream;
+	std::copy(columns.begin(), columns.end(), std::ostream_iterator<std::string>(columnStringStream, ","));
+	std::string columnString = columnStringStream.str();
+	statementStream << columnString.substr(0, columnString.size() - 1) << " FROM " << table;
+
+	if (!whereCondition.empty())
+	{
+		statementStream << " WHERE " << whereCondition;
+	}
+
+	if (!orderBy.empty())
+	{
+		statementStream << " ORDER BY " << orderBy;
+	}
+
+	if (!groupBy.empty())
+	{
+		statementStream << " GROUP BY " << groupBy;
+	}
+
+	if (limit != 0)
+	{
+		statementStream << " LIMIT " << limit;
+	}
+
+	if (!extra.empty())
+	{
+		statementStream << " " << extra;
+	}
+
+	statementStream << ";";	
+
+	char* errMessage = nullptr;
+
+	std::function<int(void * data, int argc, char ** argv, char ** colName)> callback = [&](void * data, int argc, char ** argv, char ** colName) -> int {
+		
+	};
+
+	int res = sqlite3_exec(m_db, statementStream.str().c_str(), callback, nullptr, &errMessage);
+
+	if (res != SQLITE_OK)
+	{
+		throw std::runtime_error("Failed to fetch from database: '" + std::string(errMessage) + "'");
+	}
+}
+
+int(*sqliteCallback(SqliteDatabase &db))(void * data, int argc, char ** argv, char ** colName)
+{
+	return [&db](void * data, int argc, char ** argv, char ** colName) -> int
+	{
+		std::map<std::string, std::string> row;
+		for (int i = 0; i < argc; i++)
+		{
+			row.insert(colName[i], argv[i]);
+		}
+		db.m_fetchTmp.push_back(row);
+		return 0;
+	};
 }
--- TriviaServer/SqliteDatabase.h
+++ TriviaServer/SqliteDatabase.h
@@ -2,6 +2,8 @@
 
 #include <exception>
 #include <sstream>
+#include <map>
+#include <iostream>
 #include "IDatabase.h"
 #include "Config.h"
 
@@ -16,10 +18,16 @@ class SqliteDatabase : public IDatabase
 		virtual const std::list<Question> getQuestions(int id);
 		virtual const LoggedUser loginUser(std::string username, std::string password);
 		virtual const void signupUser(std::string username, std::string password, std::string email);
+		void sqlFetch(std::string table, std::vector<std::string> columns = { "*" }, std::string whereCondition = "", std::string orderBy = "", std::string groupBy = "", int limit = 0, std::string extra = "");
 
 	private:
 		sqlite3* m_db;
+		std::vector<std::map<std::string, std::string>> m_fetchTmp;
 
 		void sqlInsert(std::string table, std::vector<std::string> columns, std::vector<std::string> values); // Values need to get converted to a string beforehand
+		// void sqlFetch(std::string table, std::vector<std::string> columns = { "*" }, std::string whereCondition = "", std::string orderBy = "", std::string groupBy = "", int limit = 0, std::string extra = "");
+
+		friend int(*sqliteCallback(SqliteDatabase &db))(void * data, int argc, char ** argv, char ** colName)
+
 };
 
