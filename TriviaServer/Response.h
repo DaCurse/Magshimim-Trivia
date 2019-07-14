81be440d49bd543b06abe776210699891ff22024
Daniel <dacurse0@gmail.com>
2019-07-14 16:57:47 +0300

    Added more request types

--- TriviaServer/Response.h
+++ TriviaServer/Response.h
@@ -5,7 +5,13 @@
 typedef enum ResponseID {
 	REQUEST_ERROR = 201,
 	LOGIN_SUCCESS = 202,
-	SIGNUP_SUCCESS = 203
+	SIGNUP_SUCCESS = 203,
+	LOGOUT_SUCCESS = 204,
+	GOT_ROOMS = 205,
+	GOT_PLAYERS = 206,
+	GOT_HIGHSCORE = 207,
+	JOINED_ROOM = 208,
+	CREATED_ROOM = 209
 } ResponseID;
 
 typedef struct LoginResponse {
@@ -15,6 +21,33 @@ typedef struct LoginResponse {
 typedef struct SignupResponse {
 	ResponseID status;
 } SignupResponse;
+
+typedef struct LogoutResponse {
+	ResponseID status;
+};
+
+typedef struct GetRoomsResponse {
+	ResponseID status;
+	// vec
+};
+
+typedef struct GetPlayersInRoomResponse {
+	ResponseID status;
+	std::vector<std::string> rooms;
+};
+
+typedef struct HighscoreResponse {
+	ResponseID status;
+	// vec
+};
+
+typedef struct JoinRoomResponse {
+	ResponseID status;
+};
+
+typedef struct CreateRoomResponse {
+	ResponseID status;
+};
 
 typedef struct ErrorResponse {
 	std::string message;
